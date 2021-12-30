7d2e66cc0d233072ced098bfcc3d9fd9929dcbcb
heyiminhye <heyiminhye@gmail.com>
2021-12-30 20:39:43 +0900

    Feat: Add UI of New.js
    헤더 타이틀, 뒤로가기 버튼 추가
    날짜 출력 및 날짜를 선택하는 섹션 추가
    오늘의 감정을 선택할 수 있는 섹션 UI 부분 추가

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,6 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import MyButton from "./MyButton";
+
 const DiaryItem = ({ id, emotion, content, date }) => {
+  const navigate = useNavigate();
   const env = process.env;
   env.PUBLIC_URL = env.PUBLIC_URL || "";
+
+  const strDate = new Date(parseInt(date)).toLocaleDateString();
+
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+
+  const goEdit = () => {
+    navigate(`/Edit/${id}`);
+  };
   return (
     <div className="DiaryItem">
       <div
@@ -11,8 +25,13 @@ const DiaryItem = ({ id, emotion, content, date }) => {
       >
         <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
       </div>
-      <div></div>
-      <div></div>
+      <div onClick={goDetail} className="info_wrapper">
+        <div className="diary_date">{strDate}</div>
+        <div className="diary_content_preview">{content.slice(0.25)}</div>
+      </div>
+      <div className="btn_wrapper">
+        <MyButton onClick={goEdit} text={"수정하기"} />
+      </div>
     </div>
   );
 };
