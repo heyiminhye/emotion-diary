dab1138d79c7201f66b53276cc6f7d76eb70fc70
heyiminhye <heyiminhye@gmail.com>
2022-01-16 19:55:06 +0900

    Use LocalStorage

--- src/App.js
+++ src/App.js
@@ -43,9 +43,25 @@ function App() {
 
   const [data, dispatch] = useReducer(reducer, []);
 
+  useEffect(() => {
+    const localData = localStorage.getItem("diary");
+    if (localData) {
+      const diaryList = JSON.parse(localData).sort(
+        (a, b) => parseInt(b.id) - parseInt(a.id)
+      );
+
+      dataId.current = parseInt(diaryList[0].id) + 1;
+
+      console.log(diaryList);
+      console.log(dataId);
+
+      dispatch({ type: "INIT", data: diaryList });
+    }
+  }, []);
+
   console.log(new Date().getTime()); //현재날짜를 ISO 형식으로 보여줌
 
-  const dataId = useRef(6);
+  const dataId = useRef(0);
   // CREATE
   const onCreate = (date, content, emotion) => {
     dispatch({
