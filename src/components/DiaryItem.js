27f4da3a8a65cf556ed3cbbf34e31a13cd3ae8ae
heyiminhye <heyiminhye@gmail.com>
2022-01-16 20:40:38 +0900

    Fix: git pages bug

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -23,7 +23,10 @@ const DiaryItem = ({ id, emotion, content, date }) => {
           ` emotion_img_wrapper_${emotion}`,
         ].join(" ")}
       >
-        <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
+        <img
+          src={process.env.PUBLIC_URL + `/assets/emotion${emotion}.png`}
+          alt=""
+        />
       </div>
       <div onClick={goDetail} className="info_wrapper">
         <div className="diary_date">{strDate}</div>
